b3ff91da0e3610d001f102f1a5f9367fd8d57e8f
Yosef Tukachinsky <yoostuk@gmail.com>
2021-02-05 01:10:21 +0200

    change login from manipulate image data to clip-path

--- js/index.js
+++ js/index.js
@@ -19,33 +19,23 @@ const freq2 = {
     max: 5,
     min: 1,
 };
-let regularImgData = [];
-let vampireImgData = [];
+// let regularImgData = [];
+// let vampireImgData = [];
 let img = new Image();
 let imgV = new Image();
 let x = 0, y = 0;
 img.onload = initImg;
-imgV.onload = initVampImg;
+// imgV.onload = initVampImg;
 const cw = (percent=1) => canvas.width*percent;
 const ch = (percent=1) => canvas.height*percent;
 const distance = (x,y, x2, y2) => Math.sqrt((x-x2)**2+(y-y2)**2);
-const getXY = (i,w) => ({dy: ((i/4)/w)|0, dx: (i/4)%w});
+// const getXY = (i,w) => ({dy: ((i/4)/w)|0, dx: (i/4)%w});
 let play = false;
 window.addEventListener('resize', handleResize);
 canvas.addEventListener('mousemove', handleMove);
 canvas.addEventListener('mouseleave', () => play = false);
 canvas.addEventListener('mouseenter', () => {play = true; frame()});
-function updateFreq(freq){
-    // freq.value += freq.speed;
-    freq.shift = (freq.shift + freq.speed);
-    // if(freq.value > freq.max) {
-    //     freq.value = freq.max;
-    //     freq.speed *= -1;
-    // } else if(freq.value < freq.min) {
-    //     freq.value = freq.min;
-    //     freq.speed *= -1;
-    // }
-}
+
 function initCanvas() {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
@@ -53,28 +43,28 @@ function initCanvas() {
 }
 function initImg(){
     ctx.drawImage(img, 0, 0, cw(), ch());
-    regularImgData = [...ctx.getImageData(0,0,cw(),ch()).data];
+//     regularImgData = [...ctx.getImageData(0,0,cw(),ch()).data];
 }
-function initVampImg(){
-    const mc = document.createElement('canvas');
-    mc.width = cw();
-    mc.height = ch();
-    const mctx = mc.getContext('2d');
-    mctx.drawImage(imgV, 0, 0, cw(), ch());
-    vampireImgData = [...mctx.getImageData(0,0,cw(),ch()).data];
-}
+// function initVampImg(){
+//     const mc = document.createElement('canvas');
+//     mc.width = cw();
+//     mc.height = ch();
+//     const mctx = mc.getContext('2d');
+//     mctx.drawImage(imgV, 0, 0, cw(), ch());
+//     vampireImgData = [...mctx.getImageData(0,0,cw(),ch()).data];
+// }
 function handleResize() {
     initCanvas();
-    initImg();
-    initVampImg();
+    // initImg();
+    // initVampImg();
 }
 function handleMove({offsetX,offsetY}) {x = offsetX;y = offsetY;}
 function frame(lastTimestamp = new Date().getTime()) {
     if(!play) {
         return ctx.drawImage(img,0,0,cw(),ch());
     }
-    if(regularImgData?.length !== ch()*cw()*4) {initImg()}
-    if(vampireImgData?.length !== ch()*cw()*4) {initVampImg()}
+    // if(regularImgData?.length !== ch()*cw()*4) {initImg()}
+    // if(vampireImgData?.length !== ch()*cw()*4) {initVampImg()}
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     const d = (deg) => {
@@ -83,22 +73,33 @@ function frame(lastTimestamp = new Date().getTime()) {
     const deg = (dx, dy) => Math.atan2(dy-totalR, dx-totalR) + Math.PI;
     ctx.drawImage(img, 0,0, cw(), ch());
     const totalR = Math.round(ch(RADIUS+RADIUS_OVER_1+RADIUS_OVER_2));
-    const imageData = ctx.getImageData(x-totalR,y-totalR,totalR*2,totalR*2);
-    const data = imageData.data;
-    for(let i=0;i<data.length;i+=4) {
-        const {dx, dy} = getXY(i, totalR*2);
-        const idx = ((y-totalR+dy)*cw() + x-totalR+dx)*4;
-        if(distance(totalR, totalR, dx, dy) > d(deg(dx, dy, totalR))) {
-            // data[i] = regularImgData[idx];
-            // data[i+1] = regularImgData[idx+1];
-            // data[i+2] = regularImgData[idx+2];
-        } else {
-            data[i] = vampireImgData[idx];
-            data[i+1] = vampireImgData[idx+1];
-            data[i+2] = vampireImgData[idx+2];
-        }
+    // const imageData = ctx.getImageData(x-totalR,y-totalR,totalR*2,totalR*2);
+    // const data = imageData.data;
+    // for(let i=0;i<data.length;i+=4) {
+    //     const {dx, dy} = getXY(i, totalR*2);
+    //     const idx = ((y-totalR+dy)*cw() + x-totalR+dx)*4;
+    //     if(distance(totalR, totalR, dx, dy) > d(deg(dx, dy, totalR))) {
+    //         // data[i] = regularImgData[idx];
+    //         // data[i+1] = regularImgData[idx+1];
+    //         // data[i+2] = regularImgData[idx+2];
+    //     } else {
+    //         data[i] = vampireImgData[idx];
+    //         data[i+1] = vampireImgData[idx+1];
+    //         data[i+2] = vampireImgData[idx+2];
+    //     }
+    // }
+    // ctx.putImageData(imageData, x-totalR, y-totalR);
+    ctx.beginPath();
+    ctx.moveTo(x+d(0), y);
+    for (let i=0;i<360;i++) {
+        const θ = i/180*Math.PI;
+        const r = d(θ);
+        ctx.lineTo(x+r*Math.cos(θ), y+r*Math.sin(θ));
     }
-    ctx.putImageData(imageData, x-totalR, y-totalR);
+    ctx.moveTo(x+d(0), y);
+    ctx.clip();
+    ctx.drawImage(imgV,0, 0, cw(), ch());
+    ctx.closePath();
     const currentTimestamp = new Date().getTime();
     const dt = (currentTimestamp-lastTimestamp)/5000;
     freq1.shift+=dt*2;
